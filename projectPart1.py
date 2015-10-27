9244c3e655ac9156a41ec9832f50c39b3bf1f1da
Si He <syhe@cs.ualberta.ca>
2015-10-27 03:37:58 -0600

    Added good_connections view table.

--- projectPart1.py
+++ projectPart1.py
@@ -18,6 +18,20 @@ def create_available_view(connection):
         if row[0] == "AVAILABLE_FLIGHTS" :
             curs.execute("DROP view available_flights")
     curs.execute("create view available_flights(flightno,dep_date, src,dst,dep_time,arr_time,fare,seats,price) as select f.flightno, sf.dep_date, f.src, f.dst, f.dep_time+(trunc(sf.dep_date)-trunc(f.dep_time)), f.dep_time+(trunc(sf.dep_date)-trunc(f.dep_time))+(f.est_dur/60+a2.tzone-a1.tzone)/24, fa.fare, fa.limit-count(tno), fa.price from flights f, flight_fares fa, sch_flights sf, bookings b, airports a1, airports a2 where f.flightno=sf.flightno and f.flightno=fa.flightno and f.src=a1.acode and f.dst=a2.acode and fa.flightno=b.flightno(+) and fa.fare=b.fare(+) and sf.dep_date=b.dep_date(+) group by f.flightno, sf.dep_date, f.src, f.dst, f.dep_time, f.est_dur,a2.tzone, a1.tzone, fa.fare, fa.limit, fa.price having fa.limit-count(tno) > 0 ")
+    connection.commit()
+    curs.close()
+
+def create_good_connections(connection):
+    #crazy shit creating the good_connections view table if it does not exist,
+    #if it already exist, drop it then create.
+    curs = connection.cursor()
+    curs.execute("SELECT view_name from user_views")
+    rows = curs.fetchall()
+    for row in rows:
+        if row[0] == "GOOD_CONNECTIONS":
+            curs.execute("DROP view good_connections")
+    curs.execute("create view good_connections (src,dst,dep_date,flightno1,flightno2, layover,price) as select a1.src, a2.dst, a1.dep_date, a1.flightno, a2.flightno, a2.dep_time-a1.arr_time, min(a1.price+a2.price) from available_flights a1, available_flights a2 where a1.dst=a2.src and a1.arr_time +1.5/24 <=a2.dep_time and a1.arr_time +5/24 >=a2.dep_time group by a1.src, a2.dst, a1.dep_date, a1.flightno, a2.flightno, a2.dep_time, a1.arr_time")
+    connection.commit()
     curs.close()
 
 # loginMenu is the first thing that the user will hit in the terminal screen, 
@@ -504,6 +518,7 @@ def main():
 
         #creates the view table of available flights upon connection
         create_available_view(connection)
+        create_good_connections(connection)
         #user login stuff here
         (loginSuccess, userEmail, userPassword, 
             airLineAgent) = loginMenu(connection)
